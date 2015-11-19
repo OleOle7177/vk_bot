532e13d9bedef8316cd9e9db12dde30ea15cc72c
OleOle7177 <ole_@bk.ru>
2015-11-19 11:50:53 +0600

    Use pg 0.17

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to,  '/home/hosting_oleole7177/projects/vk-bot'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('database.yml', 'secrets.yml', 'password.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/password.yml', 'config/vk.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
